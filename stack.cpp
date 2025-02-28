be95fbc8b67177530302553ce7acd1dcf00a73b9
hemu684 <himanshushekkharmurmu007@gmail.com>
2025-02-28 11:51:26 +0530

    stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -22,10 +22,15 @@ public:
         return head == nullptr;
     }
 
+    bool checkEmpty() {
+        return head == nullptr;
+    }
+
     void push(int new_data) {
         Node* new_node = new Node(new_data);
         if (!new_node) {
             cout << "\nStack Overflow" << endl;
+            return;
         }
         new_node->next = head;
         head = new_node;
@@ -34,6 +39,7 @@ public:
     void pop() {
         if (this->isEmpty()) {
             cout << "\nStack Underflow" << endl;
+            return;
         } else {
             Node* temp = head;
             head = head->next;
@@ -49,6 +55,16 @@ public:
             return -1; // Using -1 to indicate stack is empty
         }
     }
+
+    int size() {
+        int count = 0;
+        Node* current = head;
+        while (current != nullptr) {
+            count++;
+            current = current->next;
+        }
+        return count;
+    }
 };
 
 void displayMenu() {
@@ -56,7 +72,9 @@ void displayMenu() {
     cout << "1. Push\n";
     cout << "2. Pop\n";
     cout << "3. Peek\n";
-    cout << "4. Exit\n";
+    cout << "4. Check if Empty\n";
+    cout << "5. Get Size\n";
+    cout << "6. Exit\n";
     cout << "Enter your choice: ";
 }
 
@@ -81,6 +99,15 @@ int main() {
                 cout << "Top element is " << st.peek() << endl;
                 break;
             case 4:
+                if (st.checkEmpty())
+                    cout << "Stack is empty" << endl;
+                else
+                    cout << "Stack is not empty" << endl;
+                break;
+            case 5:
+                cout << "Stack size is " << st.size() << endl;
+                break;
+            case 6:
                 cout << "Exiting..." << endl;
                 return 0;
             default:
